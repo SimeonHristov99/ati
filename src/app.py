9059427ca195dcbc501a1dbc4ba69cc468355bab
SimeonHristov99 <s.e.hristov99@gmail.com>
2023-02-07 10:25:12 +0200

    Implement text features pipeline in app.

--- src/app.py
+++ src/app.py
@@ -1,10 +1,11 @@
 """This is the from which the web UI is created and displayed."""
 
+import numpy as np
 import pandas as pd
 import streamlit as st
 from pycaret.classification import load_model, predict_model
 
-from pipelines import pipeline_tf_idf
+from pipelines import pipeline_tf_idf, pipeline_text_features
 
 DATA_PATH_PREP = '../DATA/prepared'
 
@@ -14,7 +15,7 @@ def identity(x):
 
 
 ridge = load_model(f'{DATA_PATH_PREP}/06_pycaret_ridge')
-# et = load_model(f'{DATA_PATH_PREP}/06_pycaret_ridge_textfeats_et')
+et = load_model(f'{DATA_PATH_PREP}/06_pycaret_ridge_textfeats_et')
 
 st.set_page_config(
     layout='wide',
@@ -22,32 +23,48 @@ st.set_page_config(
     page_title='Ati'
 )
 
-
 st.title("Welcome to Ati! 📑")
 st.subheader("A system for multiclass single-label Authorship aTtrIbution")
+
+with st.sidebar:
+    add_radio = st.radio(
+        'Choose a text preprocessing method',
+        ('TF-IDF', 'Text Features', 'sBERT')
+    )
 
 text_input = st.text_area('Text to analyze', height=450)
 
 if st.button("Submit") and text_input != '':
 
     with st.spinner():
-        text_input_df_vect = pipeline_tf_idf(text_input)
-        # print(text_input_df_vect)
+        if add_radio == 'TF-IDF':
+            text_input_df_vect = pipeline_tf_idf(text_input)
+            results_ridge = predict_model(ridge, data=text_input_df_vect)
+            prediction_ridge = results_ridge['prediction_label'][0]
+            st.markdown(
+                f'The most likely author according to Ridge Classifier of the above text is **{prediction_ridge}**.')
+        elif add_radio == 'Text Features':
+            text_input_tf, tags = pipeline_text_features(
+                text_input, return_tags=True)
 
-        # Predict
-        results_ridge = predict_model(ridge, data=text_input_df_vect)
-        prediction_ridge = results_ridge['prediction_label'][0]
+            st.write(
+                'Here is a breakdown of the part-of-speech tags found in your text:')
+            st.write(tags)
 
-        # results_et = predict_model(et, data=text_input_df_vect, )
-        # print(results_et.columns)
-        # prediction_et = results_et['prediction_label'][0]
-
-    # st.write('Done')
-    st.markdown(
-        f'The most likely author according to ridge of the above text is **{prediction_ridge}**.')
-
-    # st.markdown(
-    #     f'The most likely author according to et of the above text is **{prediction_et}**.')
+            st.write(
+                'Here is the complexity of the text based on different metrics:')
+            metrics = ['fre', 'air', 'gfi', 'cli', 'smog']
+            st.write(text_input_tf[metrics])
+            
+            text_input_tf.columns = np.arange(len(text_input_tf.columns))
+            results_et = predict_model(et, data=text_input_tf)
+            prediction_et = results_et['prediction_label'][0]
+            st.markdown(
+                f'The most likely author according to Extra Trees Classifier of the above text is **{prediction_et}**.')
+        elif add_radio == 'sBERT':
+            pass
+        else:
+            st.error('This type of text preprocessing is not supported, yet.')
 
     # y_pred_class = model.predict(X_test) # Used in evalution
     # pipe_svc.predict(tmp)
